17b2cb08465b3691fd8b1c19d50270fbf45e14c7
dannitt <116494171+dannitt@users.noreply.github.com>
2022-10-23 15:08:36 +0300

    Add files via upload

---
+++ JS2/greaterNumber.js
@@ -0,0 +1,10 @@
+function greaterNumber(input){
+    const a = Number(input[0]);
+    const b = Number(input[1]);
+if (a > b){
+    console.log(a);
+} else {
+    console.log(b);
+}
+}
+greaterNumber(["-8", "6"]);
