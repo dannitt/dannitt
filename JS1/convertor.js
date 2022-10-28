e50ee067efee02c2f04827fb7af6759589d300be
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:34:36 +0300

    Delete convertor.js

--- JS1/convertor.js
+++
@@ -1,7 +0,0 @@
-function convertor(input){
-    let amountUsd = Number(input[0]);
-    let course = Number(1.79549);
-    let amountBgl = amountUsd * course;
-        console.log(`${amountBgl}`);
-    }
-    convertor([22])
