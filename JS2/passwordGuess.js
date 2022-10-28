f11eb334511b5893787327a38be1552d3bb61b8e
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:42:21 +0300

    Delete passwordGuess.js

--- JS2/passwordGuess.js
+++
@@ -1,9 +0,0 @@
-function passwordGuess(input){
-    let text = input[0];
-if (text === "s3cr3t!P@ssw0rd") {
-    console.log("Welcome");
-} else {
-    console.log("Wrong password!")
-}
-}
-passwordGuess(["querty"])
