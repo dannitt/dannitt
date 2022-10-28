1178ff8cc49e531c5e42e2856f1626c7200ada12
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 09:46:30 +0300

    Delete censoredWords.js

--- JSF9/censoredWords.js
+++
@@ -1,8 +0,0 @@
-function censoredWords(sentence, word) {
-    
-    let words = sentence.split(word)
-    let res = words.join("*".repeat(word.length))
-    console.log( res)
-    }
-
-censoredWords('A small sentence with some words', 'small')
