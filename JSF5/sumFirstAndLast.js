a33163d8cc7526df2284f487fc65e4e73782253b
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 09:41:22 +0300

    Delete sumFirstAndLast.js

--- JSF5/sumFirstAndLast.js
+++
@@ -1,8 +0,0 @@
-function sumFirstAndLast(arr) {
-let res = 0
-    let firstNum = Number (arr[0])
-    let lastNum = Number (arr.pop())
-res = firstNum + lastNum
-    console.log (res)
-}
-sumFirstAndLast(['20', '30', '40'])
