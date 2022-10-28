b6bd6f78de26f6f2ad9221fa286dd8114c7bc6fd
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:57:00 +0300

    Delete echo.js

--- JSF2/echo.js
+++
@@ -1,9 +0,0 @@
-function echo(param) {
-console.log(typeof(param));
-if (typeof(param) === "string" || typeof(param) === "number"){
-    console.log(param);
-} else {
-    console.log('Parameter is not suitable for printing')
-}
-}
-echo(11)
