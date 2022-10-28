95f63d92e0b4da32d0d12cbc7cb06f68124cda41
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:36:25 +0300

    Delete trapets.js

--- JS1/trapets.js
+++
@@ -1,8 +0,0 @@
-function trapets (input){
-const a = Number (input[0]);
-const b = Number (input[1]);
-const h = Number (input[2]);
-const amnt = (a + b) * h / 2;
-console.log(amnt.toFixed(2));
-}
-trapets(["8","13","7"]);
