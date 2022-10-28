3a7375f55e903630fca598dbebef47224f665077
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 09:46:24 +0300

    Delete 7cutAndReverse.js

--- JSF9/7cutAndReverse.js
+++
@@ -1,8 +0,0 @@
-function cutAndReverse(input) {
-    let reversed = input.split("").reverse().join("")
-    let sliceOne = reversed.slice(reversed.length/2)
-    let sliceTwo = reversed.slice(0,reversed.length/2)
-console.log(sliceOne)
-console.log(sliceTwo)
-}
-cutAndReverse('tluciffiDsIsihTgnizamAoSsIsihT')
