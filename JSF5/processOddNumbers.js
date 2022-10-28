150e618b25049fbb1ea9c0020349dd87aa74fa1f
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 09:41:09 +0300

    Delete processOddNumbers.js

--- JSF5/processOddNumbers.js
+++
@@ -1,7 +0,0 @@
-function processOddNumbers(arr) {
-let oddNum = arr.filter((el, i)=> i%2 === 1)
-            .map(x => x*2)
-            .reverse()
-console.log(oddNum.join(" "))
-}
-processOddNumbers([10, 15, 20, 25])
