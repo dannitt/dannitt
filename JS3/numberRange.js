18e60e41718808d17f48c2180ca8d20406f85ba1
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:44:02 +0300

    Delete numberRange.js

--- JS3/numberRange.js
+++
@@ -1,9 +0,0 @@
-function numberRange(input) {
-    const number = Number(input[0]);
-if (number >= -100 && number <= 100 && number !== 0) {
-    console.log("Yes")
-} else {
-    console.log("No");
-}
-}
-numberRange(["0"]);
