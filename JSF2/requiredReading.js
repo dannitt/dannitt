d70fd916c649a1d6d2ceb7c06197509b913f3875
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:57:17 +0300

    Delete requiredReading.js

--- JSF2/requiredReading.js
+++
@@ -1,6 +0,0 @@
-function requiredReading(numPages, pagesPerHour, days) {
-let pagesPerDay = numPages / days;
-let hoursPerDay = pagesPerDay / pagesPerHour
-    console.log(hoursPerDay)
-}
-requiredReading(432, 15 , 4)
