80cffcd70e56decdc81f0c5628cefed1fc3a3b7e
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:56:50 +0300

    Delete centuriesToMinutes.js

--- JSF2/centuriesToMinutes.js
+++
@@ -1,8 +0,0 @@
-function centuriesToMinutes(cent) {
-    let years = cent * 100;
-    let allDays = Math.floor (years* 365.2422);
-    let hours = (allDays) * 24;
-    let minutes = hours * 60;
-    console.log(`${cent} centuries = ${years} years = ${allDays} days = ${hours} hours = ${minutes} minutes`)
-    }
-centuriesToMinutes(1)
