f99c5e98e0ebf87e56039592ab75bdc1556cd051
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 09:40:05 +0300

    Delete 5sortAnArrayBy2Criteria.js

--- JSF5/5sortAnArrayBy2Criteria.js
+++
@@ -1,9 +0,0 @@
-function sortAnArrayBy2Criteria(arr) {
-    let sortedArr = arr.sort((a, b) => {
-        return a.length - b.length || a.localeCompare(b);
-    });
-    for (let el of sortedArr) {
-        console.log(el)
-    }
-}
-sortAnArrayBy2Criteria(['alpha', 'beta', 'gamma'])
