acf5d62cea5cb413a7bed742a42c60c9dff71632
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 09:08:46 +0300

    Delete reverseAnArrayOfNumbers.js

--- JSF3/reverseAnArrayOfNumbers.js
+++
@@ -1,9 +0,0 @@
-function reverseAnArrayOfNumbers (n, arr) {
-let arrNew = []
-for ( let i=n-1; i>=0; i-- ){
-    arrNew.push(arr[i])
-    
-}
-console.log(arrNew.join(" "))
-}
-reverseAnArrayOfNumbers(4, [-1, 20, 99, 5])
