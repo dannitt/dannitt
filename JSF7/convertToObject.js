e5ba624c7e403bc1c5eae22510ac79de2ea97872
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 09:42:29 +0300

    Delete convertToObject.js

--- JSF7/convertToObject.js
+++
@@ -1,7 +0,0 @@
-function convertToObject(objJ) {
-let obj = JSON.parse(objJ)
-for (let key of Object.keys(obj)){
-    console.log(`${key}: ${obj[key]}`)
- }
-}
-convertToObject('{"name": "George", "age": 40, "town": "Sofia"}')
