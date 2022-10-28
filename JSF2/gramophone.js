c170fb7c7cc8cb919bdf423283c7df7f4f85b007
Yordanka Georgieva <116494171+dannitt@users.noreply.github.com>
2022-10-28 08:57:06 +0300

    Delete gramophone.js

--- JSF2/gramophone.js
+++
@@ -1,5 +0,0 @@
-function gramophone(band, album, song) {
-let rotation = ((album.length * band.length) * song.length / 2 )/ 2.5;
-    console.log(`The plate was rotated ${Math.ceil(rotation)} times.`)
-}
-gramophone('Black Sabbath', 'Paranoid', 'War Pigs')
